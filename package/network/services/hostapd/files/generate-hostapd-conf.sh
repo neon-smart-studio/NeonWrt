de52c9f00013ed2b5ef44af5ff63c188dd716e21
Neon Cat Studio <sweetcat840202@gmail.com>
2025-05-24 20:21:22 +0800

    hostpad up & AP startup

--- package/network/services/hostapd/files/generate-hostapd-conf.sh
+++ package/network/services/hostapd/files/generate-hostapd-conf.sh
@@ -9,14 +9,23 @@ if [ -z "$WLAN_IFACE" ]; then
     exit 1
 fi
 
+# 取得 MAC 後 3 bytes，格式例如：334455
+MAC=$(cat /sys/class/net/"$WLAN_IFACE"/address | tr -d ':')
+MAC_SUFFIX="${MAC:6:6}"  # 例如 334455
+SSID="NeonWrt-${MAC_SUFFIX}"
+
 mkdir -p /var/run
+
 cat <<EOF > "$CONF_FILE"
 interface=$WLAN_IFACE
 driver=nl80211
-ssid=OpenWrt-FreeWiFi
-channel=6
+ssid=$SSID
 hw_mode=g
+channel=6
+ieee80211n=1
+wmm_enabled=1
 auth_algs=1
+ignore_broadcast_ssid=0
 EOF
 
 chown network:network "$CONF_FILE"
